f3db5226de744f55466f880db4f8a078ca142465
Glitch (fcc-infosec-issue-tracker-fred) <none>
2018-11-04 20:41:03 +0000

    code style rework

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const expect      = require('chai').expect;
-const cors        = require('cors');
-
+const express           = require('express');
+const bodyParser        = require('body-parser');
+const expect            = require('chai').expect;
+const cors              = require('cors');
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
 const runner            = require('./test-runner');
+const helmet            = require("helmet");
 
 const app = express();
 
@@ -15,21 +15,17 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(cors({origin: '*'})); //For FCC testing purposes only
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// --- helmet
-const helmet = require("helmet");
 app.use(helmet.xssFilter());
-// ---
 
 //Sample front-end
 app.route('/:project/')
-  .get(function (req, res) {
+  .get((req, res) => {
     res.sendFile(process.cwd() + '/views/issue.html');
   });
 
 //Index page (static HTML)
 app.route('/')
-  .get(function (req, res) {
+  .get((req, res) => {
     res.sendFile(process.cwd() + '/views/index.html');
   });
 
@@ -40,22 +36,22 @@ fccTestingRoutes(app);
 apiRoutes(app);  
     
 //404 Not Found Middleware
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.status(404)
-    .type('text')
-    .send('Not Found');
+     .type('text')
+     .send('Not Found');
 });
 
 //Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port " + process.env.PORT);
-  if(process.env.NODE_ENV==='test') {
+  if (process.env.NODE_ENV==='test') {
     console.log('Running Tests...');
-    setTimeout(function () {
+    setTimeout(() => {
       try {
         runner.run();
       } catch(e) {
-        var error = e;
+        const error = e;
           console.log('Tests are not valid:');
           console.log(error);
       }
