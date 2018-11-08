3a41786d46158050b5124f1d7fe7ed0479602f71
Glitch (fcc-infosec-issue-tracker-fred) <none>
2018-11-08 22:22:47 +0000

    updated styling and layout

--- routes/api.js
+++ routes/api.js
@@ -14,10 +14,6 @@ MongoClient.connect(CONNECTION_STRING, (err, db) => {
 module.exports = app => {
   app.route('/api/issues/:project')
     .get((req, res) => {
-      // for an array of all issues on that specific project with all the information for each issue as was returned when posted.
-      // I can filter my get request by also passing along any field and value in the query(ie. /api/issues/{project}?open=false).
-      // I can pass along as many fields/values as I want.
-      
       const project = req.params.project;
       const query = req.query;
       query.project = project;
@@ -34,10 +30,7 @@ module.exports = app => {
     
     .post((req, res) => {
       const project = req.params.project;
-      // with form data containing required issue_title, issue_text, created_by, and optional assigned_to and status_text.
-      // The object saved (and returned) will include all of those fields (blank for optional no input) 
-      // and also include created_on(date/time), updated_on(date/time), open(boolean, true for open, false for closed), and _id.
-    
+      
       MongoClient.connect(CONNECTION_STRING, (err, db) => {
         const collection = db.collection("issues");
         
@@ -66,10 +59,6 @@ module.exports = app => {
     })
     
     .put((req, res) => {
-      // with a _id and any fields in the object with a value to object said object. 
-      // Returned will be 'successfully updated' or 'could not update '+_id.
-      // This should always update updated_on. If no fields are sent return 'no updated field sent'.
-    
       let project = req.params.project;
       let changes = {};
       let noFields = true;
@@ -100,9 +89,6 @@ module.exports = app => {
     })
     
     .delete((req, res) => {
-      // with a _id to completely delete an issue.
-      // If no _id is sent return '_id error', success: 'deleted '+_id, failed: 'could not delete '+_id.
-    
       const project = req.params.project;
     
       MongoClient.connect(CONNECTION_STRING, (err, db) => {
